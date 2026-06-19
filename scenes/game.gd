b08e1942145ea744f295d93af55e4b601ec7c920
plouaincet <maiasswag@gmail.com>
2026-06-19 15:12:14 +0300

    added scary sound, tested with rng

---
+++ scenes/game.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var rng=RandomNumberGenerator.new()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var number=rng.randi_range(0,5000)
+	if number==1:
+		$AudioStreamPlayer2D.play()
+		
