737b21fb774368fa7bd78a9dfa86f4cda0ca2ea8
plouaincet <maiasswag@gmail.com>
2026-06-19 16:29:09 +0300

    Revert "added sounds and on-screen ON/OFF text to toggle-able light"
    This reverts commit 0ec6b863a707d0e1def22d6d62fa0aeb1f14d8f8.

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 50
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left","right","up","down")
 	velocity=direction*SPEED
 	if Input.is_action_just_pressed("light_toggle"):
