a8a13393d40579a4bacbb3dac60f730e528e3d64
plouaincet <maiasswag@gmail.com>
2026-06-19 14:41:42 +0300

    added maze tilemap, player+basic movement and toggle-able light

---
+++ scenes/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 50
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left","right","up","down")
+	velocity=direction*SPEED
+	if Input.is_action_just_pressed("light_toggle"):
+		$PointLight2D.visible=!$PointLight2D.visible
+	move_and_slide()
