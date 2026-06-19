3ff592dc2e662c35f0c115837221edefc44a624d
plouaincet <maiasswag@gmail.com>
2026-06-19 16:33:46 +0300

    left horror sound unused, will change in the future

--- scenes/game.gd
+++ scenes/game.gd
@@ -8,7 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var number=rng.randi_range(0,5000)
-	if number==1:
-		$Horror_sound.play()
+	pass
 		
