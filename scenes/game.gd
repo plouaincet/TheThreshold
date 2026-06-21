4845cac8b277f33d16295106795b1b7efd623d12
plouaincet <maiasswag@gmail.com>
2026-06-21 18:49:41 +0300

    added enemy navigation AI and sound when enemy is visible on screen

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,12 +1,22 @@
 extends Node
 
 var rng=RandomNumberGenerator.new()
+var frame_cnt:int=1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Static.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-		
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	$Horror_sound.play()
+	$player.SPEED+=30
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	$player.SPEED-=30
